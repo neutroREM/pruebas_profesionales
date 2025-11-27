7c809369682f48551fb199842540583f68bfb21e
Raúl Estrada Mejia <restrada@aslogic.mx>
2025-11-27 13:57:31 -0600

    Funcionalidades mas

---
+++ ServerAPP - RepositoresStrucutre - Example/Helper/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+
+namespace certinom_sadper_v1.Helper
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask Inactividad<T>(this IJSRuntime js, IJSObjectReference modulo,
+            DotNetObjectReference<T> dotNetObjectReference) where T : class
+        {
+            //IJSObjectReference? module = await js.InvokeAsync<IJSObjectReference>("import", "./Shared/MainLayout.razor.js");
+            await modulo.InvokeVoidAsync("Inactividad", dotNetObjectReference);
+        }
+        public static async ValueTask AlertaConfirmacion<T>(this IJSRuntime js, IJSObjectReference modulo, string sAdminAccion,
+            DotNetObjectReference<T> dotNetObjectReference) where T : class
+        {
+            await modulo.InvokeVoidAsync($"{sAdminAccion}MensajeConfirmacion", dotNetObjectReference);
+        }
+
+
+        public static void GuardarComo(this IJSRuntime js, string nombreArchivo, byte[] ayArchivo)
+        {
+            js.InvokeAsync<object>("saveAsFile",
+                nombreArchivo,
+                Convert.ToBase64String(ayArchivo));
+        }
+
+        public static void AbirDocumento(this IJSRuntime js, byte[] ayArchivo, string sMetodo)
+        {
+            js.InvokeAsync<object>(sMetodo, ayArchivo);
+        }
+    }
+}
